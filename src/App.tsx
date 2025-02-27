1abc81b992f15a7f75b01f2cb21bc81fb35a506c
Dassana <dassana002@gmail.com>
2025-02-27 21:48:55 +0530

    Added NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello World</h1>
+        <NavBar/>
     </div>
   );
 }

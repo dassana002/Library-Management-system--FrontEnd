9c0d959e948a895da569cb89dc90391f01e78e8c
Dassana <dassana002@gmail.com>
2025-02-28 11:09:04 +0530

    Populate data to the table

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -48,25 +48,20 @@ export const Book = () => {
             <Table striped="columns">
                 <thead>
                     <tr>
-                        <th> Number </th>
+                        {/* <th> Number </th> */}
                         {tHeadings.map((headings)=>(
                             <th>{headings}</th>
                         ))}
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
+                    {books.map((row)=>(
+                        <tr key={row.bookId}>
+                            {Object.values(row).map((cell,index)=> (
+                                <td key={index}>{cell}</td>
+                            ))}
+                        </tr>
+                    ))}
                 </tbody>
             </Table>
         </div>

5d6586efc3b67d5a301d643fba57c22bc3a014d3
Dassana <dassana002@gmail.com>
2025-03-14 13:08:45 +0530

    Update and Delete Button Add to Staff

--- src/components/staff/Staff.tsx
+++ src/components/staff/Staff.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import Table from "react-bootstrap/esm/Table";
 import { GetAllStaff } from "../../service/Staff";
+import Button from "react-bootstrap/esm/Button";
 
 export const Staff = () => {
 
-    const tHeadings :string[] = [
+    const tHeadings: string[] = [
         "#",
         "StaffId",
         "FirstName",
@@ -17,52 +18,69 @@ export const Staff = () => {
     ];
 
     interface Staff {
-        staffId :string;
-        firstName :string;
-        lastName :string;
-        email :string;
-        role :string;
+        staffId: string;
+        firstName: string;
+        lastName: string;
+        email: string;
+        role: string;
     }
 
-    const [staffs , setStaff] = useState<Staff[]>([]);
+    const [staffs, setStaff] = useState<Staff[]>([]);
 
-    useEffect(()=> {
-        const loadData = async()=> {
+    useEffect(() => {
+        const loadData = async () => {
             const getAll = await GetAllStaff();
             setStaff(getAll);
-        }    
+        }
 
         loadData();
     })
 
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = () => {
+
+    }
+
     return (
         <div style={{ maxHeight: "2000px", overflow: "auto", border: "2px solid #ddd" }}>
             <Table striped="columns" style={{ minWidth: "600px", borderCollapse: "separate" }}>
-                
+
                 <thead style={{ position: "sticky", top: 0, background: "#fff", zIndex: 2 }}>
                     <tr>
-                        {tHeadings.map((Header)=>(
-                           <th>{Header}</th>     
+                        {tHeadings.map((Header) => (
+                            <th>{Header}</th>
                         ))}
                     </tr>
                 </thead>
-                
+
                 <tbody>
-                    {staffs.map((row,index)=>
-                        
+                    {staffs.map((row, index) =>
+
                         <tr key={row.staffId}>
-                            <td>{index+1}</td>
-                            {Object.values(row).map((cell,index)=>
+                            <td>{index + 1}</td>
+                            {Object.values(row).map((cell, index) =>
                                 <td key={index}>{cell}</td>
                             )}
-                        </tr>    
+                            <th>
+                                <Button variant="outline-secondary"
+                                    onClick={handleEdit}
+                                > Edit </Button>
+                                <Button variant="outline-danger"
+                                    onClick={handleDelete}
+                                > Delete </Button>
+                            </th>
+                        </tr>
                     )}
+
                 </tbody>
 
                 <tfoot style={{ position: "sticky", bottom: 0, background: "#fff", zIndex: 2 }}>
                     <tr>
                         <td colSpan={tHeadings.length + 2}
-                                style={{ textAlign: "center", fontWeight: "bold", padding: "10px" }}></td>
+                            style={{ textAlign: "center", fontWeight: "bold", padding: "10px" }}></td>
                     </tr>
                 </tfoot>
             </Table>

d6a88a1b14aa168f7e1f99df2d93e43cb5874e2c
Dassana <dassana002@gmail.com>
2025-03-16 18:55:59 +0530

    Added Signin Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,23 @@ import { Lending } from "./components/lending/Lending";
 import { Member } from "./components/member/Member";
 import { NavBar } from "./components/NavBar";
 import { Staff } from "./components/staff/Staff";
+import { Signin } from "./components/user/Signin";
 
 function App() {
   return (
-      <div>
-        <BrowserRouter>
+      <div className="App">
+        <Signin/>
+        {/* <BrowserRouter>
           <NavBar/>
             <Route path="/" element={<Book/>}/>
             <Route path="/book" element={<Book/>}/>
             <Route path="/member" element={<Member/>}/>
             <Route path="/staff" element={<Staff/>}/>
             <Route path="/lending" element={<Lending/>}/>
-        </BrowserRouter>
+        </BrowserRouter> */}
+
+
+        
       </div>
   );
 }

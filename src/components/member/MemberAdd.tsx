eea62c7587ebe980c3ae1377cd2c4e8e71eb4f8d
Dassana <dassana002@gmail.com>
2025-03-21 13:36:38 +0530

    Member Add Form TextFeild Ready

--- src/components/member/MemberAdd.tsx
+++ src/components/member/MemberAdd.tsx
@@ -1,11 +1,13 @@
 import Button from "react-bootstrap/esm/Button";
+import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
+import Form from "react-bootstrap/esm/Form";
 import Modal from "react-bootstrap/esm/Modal";
 
-interface Pops{
-    show :boolean;
+interface Pops {
+    show: boolean;
 }
 
-export const MemberAdd = ({show} :Pops) => {
+export const MemberAdd = ({ show }: Pops) => {
     return (
         <div
             className="modal show"
@@ -15,11 +17,43 @@ export const MemberAdd = ({show} :Pops) => {
                 show={show}
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>Member Add</Modal.Title>
+                    <Modal.Title> Add Form </Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <p>Modal body text goes here.</p>
+                <Form>
+                    <FloatingLabel controlId="floatingInput" label="Member Id">
+                        <Form.Control 
+                            name="memberId" 
+                            placeholder="Member Id" 
+                            size="sm"
+                        />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingInput" label="First Name">
+                        <Form.Control 
+                            name="firstname" 
+                            placeholder="First Name" 
+                            size="sm" 
+                        />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingPassword" label="Last Name">
+                        <Form.Control 
+                            name="lastname" 
+                            placeholder="Last Name" 
+                            size="sm" 
+                        />
+                    </FloatingLabel>
+
+                    <FloatingLabel controlId="floatingPassword" label="Email">
+                        <Form.Control 
+                            name="email" 
+                            placeholder="Email" 
+                            size="sm" 
+                        />
+                    </FloatingLabel>
+                </Form>
                 </Modal.Body>
 
                 <Modal.Footer>

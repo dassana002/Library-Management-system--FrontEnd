89326c36a1c199d3e762fbefee5d462e68c8e291
Dassana <dassana002@gmail.com>
2025-04-05 12:53:11 +0530

    Create Auth Provider

--- src/components/user/Signin.tsx
+++ src/components/user/Signin.tsx
@@ -34,9 +34,9 @@ export const SignIn = () => {
     //API req
     e.preventDefault();
     console.log(JSON.stringify(signIn))
-    //handleReset()
     const token = await SignInReq(signIn);
     console.log(signIn);
+    handleReset();
   }
   return (
     <>
